83dd696fecaf49d50c49243a6162565b0eeb11b2
Gopikanarra <111735604+Gopikanarra@users.noreply.github.com>
2024-07-22 20:11:59 +0530

    Add files via upload

---
+++ ServletConfigDemo/src/main/java/com/servletconfig/ServletConfigHere.java
@@ -0,0 +1,52 @@
+package com.servletconfig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServletConfigHere extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ServletConfigHere() {
+        super();
+       System.out.println("servlet instantiation");
+    }
+
+	public void init()throws ServletException{
+		System.out.println("servlet initialization..");
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		pw.println("<html><head><title>Output:</title></head>");
+		pw.println("<body align='center'>");
+		pw.println("<table border='1'");
+		pw.println("<tr><th>ParameterName</th><th>ParameterValue</th></tr>");
+		ServletConfig config=getServletConfig();
+		
+		Enumeration<String> paranames=config.getInitParameterNames();
+		System.out.println(paranames);
+		
+		while(paranames.hasMoreElements()) {
+			String paraname=(String)paranames.nextElement();
+			String paravalue=config.getInitParameter(paraname);
+			pw.println("<tr>");
+			pw.println("<td>"+paraname+"</td><td>"+paravalue+"</td>");
+			pw.println("</tr>");
+		}
+		pw.println("<tr><th>Company</th><th>"+config.getInitParameter("company")+"</th></tr>");
+		pw.print("</table></body></html>");
+		
+	}
+
+}
